e2331dc19ce692bc3abe8537c9c092df7f424732
David Valenzuela <dvalenzuelachang@gmail.com>
2024-03-20 18:44:05 -0700

    Se agregó el modo oscuro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import RandomUsers from './components/RandomUsers'
+import DarkMode from './components/DarkMode'
 
 function App() {
 
   return (
-    <main className='flex  justify-center items-center h-screen bg-slate-300'>
-      <div className=' h-auto w-11/12 flex flex-wrap justify-center gap-10 p-5'>
+    <main className='flex  flex-col justify-center items-center h-screen bg-slate-300 dark:bg-slate-800'>
+      <DarkMode/>
+      <div className=' h-auto w-11/12 flex flex-wrap justify-center gap-10 p-5 pt-3'>
         <RandomUsers/>
       </div>
     </main>
